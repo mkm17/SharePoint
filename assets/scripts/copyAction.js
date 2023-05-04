39eb011aab95e790e1fb3ff95f53a5525b0247c6
mkm17 <kornetmichal@gmail.com>
2023-05-04 16:55:48 +0200

    update image add button to copy power automate action (test)

---
+++ assets/scripts/copyAction.js
@@ -0,0 +1,21 @@
+var buttonClick = (object) => {
+  let nextElem = object.parentElement.nextElementSibling;
+
+  while (nextElem && !nextElem.classList.contains("code")) {
+    nextElem = nextElem.nextElementSibling;
+  }
+
+  if (nextElem) {
+    console.log("Found next element with class 'code':", nextElem);
+    var tempTextarea = document.createElement("textarea");
+    tempTextarea.value = nextElem.innerText;
+    console.log(nextElem.innerText)
+    document.body.appendChild(tempTextarea);
+    tempTextarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempTextarea);
+
+  } else {
+    console.log("No next element with class 'code' found.");
+  }
+};
